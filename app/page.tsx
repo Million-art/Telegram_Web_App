c97d38e732f5a824c306bc9e33e8614ad6d964c6
million <74927604+Million-art@users.noreply.github.com>
2024-06-30 04:05:20 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
 import Image from "next/image";
+import { retrieveLaunchParams } from "@tma.js/sdk";
+
+interface InitData {
+  [key: string]: string;
+}
 
 export default function Home() {
+  const { initDataRaw, initData } = retrieveLaunchParams();
+
+  if (!initDataRaw || !initData) {
+    return (
+      <main>
+        <section>
+          <h1>mella web app coming soon</h1>
+          <p>No launch parameters available.</p>
+        </section>
+      </main>
+    );
+  }
+
+  const initDataDecoded: InitData = JSON.parse(decodeURIComponent(initDataRaw));
+
   return (
     <main>
       <section>
-          <h1>mella web app coming soon</h1>
+        <h1>mella web app coming soon</h1>
+        {Object.entries(initDataDecoded).map(([key, value]) => (
+          <p key={key}>
+            {key}: {value}
+          </p>
+        ))}
       </section>
     </main>
   );
